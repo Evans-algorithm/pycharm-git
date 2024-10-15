b93a1e6ce5d5162bfda8a63e9a070a8b69eb3144
gavin-edward <gavin.edward0521@outlook.com>
2024-10-15 15:04:33 +0800

    hot-fix commit modified-2

--- main.py
+++ main.py
@@ -17,5 +17,6 @@ if __name__ == '__main__':
     print("code code ...")
     print("test git ...")
     print("hot-fix merge to master...")
+    print("hot-fix modified.")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
